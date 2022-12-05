26e4550bd56216d154ae87d353948ad9fbe952ff
Ian Baron <ian.baron@manchester.ac.uk>
2022-12-05 14:25:58 +0000

    perimiter uses line2d

--- perimiter/perimiter.gd
+++ perimiter/perimiter.gd
@@ -1,20 +1,29 @@
 extends Node2D
 
-@export var radius: int = 3000
+@export var radius: int = 3000 
 @export var width: float = 2.0
 @export var center = Vector2.ZERO
 @export_range(5, 256) var quality: int =32
+@onready var player_collider: CollisionPolygon2D = $PlayerCollider/CollisionPolygon2D
+
+var inner_circle: PackedVector2Array = []
 
 func _ready():
-	# create the collision circle (perimiter) for the player's characterbody2d
-	var player_collider: CollisionPolygon2D = $PlayerCollider.get_child(0)
+	set_perimeter()
+
+
+func set_perimeter(new_width: float = width, new_radius: int = radius):
+	radius = new_radius
+	width = new_width
+	# update the collision circle (perimiter) for the player's characterbody2d	
 	var points = get_polygon_points()
 	player_collider.polygon = points
-	# create the bullet deadzone
+	# update the bullet deadzone
 	$PerimiterArea/CollisionShape2D.shape.radius = radius
-	# create the polygon border
-	$BorderPolygon.polygon = points
-
+	# update the polygon border
+	$Border.set_points(inner_circle)
+	$Border.set_width(width)	
+	
 func _on_perimiter_area_area_exited(area):
 	if area.is_in_group("Bullets"):
 		area.die()
@@ -22,7 +31,7 @@ func _on_perimiter_area_area_exited(area):
 
 func get_polygon_points():
 	# based on https://github.com/angrykoala/godot-donut-collision-polygon-2d
-	var inner_circle=get_circle_points()
+	inner_circle=get_circle_points()
 	var outer_circle=get_circle_points(width)
 	inner_circle.reverse()
 	var points: PackedVector2Array = []
