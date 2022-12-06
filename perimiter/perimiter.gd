9b787071ad960a79315c2fee7fcd251486dc851c
Ian Baron <uv.fragm@gmail.com>
2022-12-06 18:27:02 +0000

    cleanup

--- perimiter/perimiter.gd
+++ perimiter/perimiter.gd
@@ -1,10 +1,10 @@
 @tool
 extends Node2D
 
-@export var radius: int = 3000
+@export var radius: int = 1400
 @export var width: float = 2.0
 @export var center = Vector2.ZERO
-@export_range(5, 256) var quality: int =32
+@export_range(5, 256) var number_of_segments: int =32
 
 
 func _ready():
@@ -12,26 +12,25 @@ func _ready():
 
 
 func set_perimeter():
-	# update the collision circle (perimiter) for the player's characterbody2d	
+	# update the collision circle (perimiter) for the player's characterbody2d
 	var points = get_polygon_points()
 	$PlayerCollider/CollisionPolygon2D.polygon = points
 	# update the bullet deadzone
 	$PerimiterArea/CollisionShape2D.shape.radius = radius
 	# update the line2d border
 	$Border.set_points(points)
-	$Border.set_width(width)	
+	$Border.set_width(width)
 
 
 func _on_perimiter_area_area_exited(area):
 	if area.is_in_group("Bullets"):
 		area.die()
-	pass
 
 
 func get_polygon_points():
 	var circle_points: PackedVector2Array = []
-	for i in range(quality + 1):
-		var angle_point = deg_to_rad(i * 360.0 / quality - 90)
+	for segment in range(number_of_segments + 1):
+		var angle_point = deg_to_rad(segment * 360.0 / number_of_segments - 90)
 		circle_points.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * (radius))
 	circle_points.reverse()
 	circle_points.append(circle_points[0]+Vector2(0.0001, 0.0001))
