884985466593b8258ea37e873832d4a0112ffb1c
Ian Baron <uv.fragm@gmail.com>
2022-12-19 17:53:55 +0000

    add new spawn location

--- enemies/spawner.gd
+++ enemies/spawner.gd
@@ -1,15 +1,13 @@
 extends Node2D
 
 enum SpawnType {
-	Points,
-	Random,
-	Circle,
-	Offscreen
+	Perimeter,
+	Random
 }
 
 @export var enemy_scene: PackedScene
 @export var spawn_count: int = 10
-@export var spawn_type: SpawnType = SpawnType.Random
+@export var spawn_type: SpawnType = SpawnType.Perimeter
 
 
 func spawn():
@@ -23,5 +21,6 @@ func spawn():
 func get_spawn_positions():
 	var new_position: Vector2
 	match spawn_type:
+		SpawnType.Perimeter: new_position = Globals.get_random_position_on_perimeter()
 		SpawnType.Random: new_position = Globals.get_random_position_within_perimeter()
 	return new_position
--- globals/globals.gd
+++ globals/globals.gd
@@ -1,17 +1,26 @@
 extends Node
 
 var perimeter
+var radius
 
 func get_perimeter() -> void:
 	perimeter = get_tree().get_first_node_in_group("Perimeter")
+	radius = perimeter.radius
+
+
+func get_random_position_on_perimeter() -> Vector2:
+	get_perimeter()
+	var angle = randf_range(0.0, 2.0 * PI)
+	var x = radius * cos(angle)
+	var y = radius * sin(angle)
+	return Vector2(x, y)
 
 
 func get_random_position_within_perimeter() -> Vector2:
 	get_perimeter()
-	var radius = perimeter.radius
 	var angle = randf_range(0.0, 2.0 * PI)
-	var x = radius * cos(angle)
-	var y = radius * sin(angle)
+	var x = randf_range(0, radius) * cos(angle)
+	var y = randf_range(0, radius) * sin(angle)
 	return Vector2(x, y)
 
 
